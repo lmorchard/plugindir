1901776c34c6401e9f5da1cfc82e9f29a5ea87f3
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-12-21 22:25:36 +0000

    Dropping the short tags
    git-svn-id: https://svn.mozilla.org/projects/plugindir/trunk@58358 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/auth_profiles/changeemail_email.php
+++ application/views/auth_profiles/changeemail_email.php
@@ -1,11 +1,11 @@
 From: l.m.orchard@gmail.com
-Subject: Email address change for <?=$login_name?>.
+Subject: Email address change for <?php echo $login_name?>.
 
 Someone (possibly you) has triggered an attempt to change the email address for 
-a login named "<?=$login_name?>".  If you are that someone, follow this link to 
+a login named "<?php echo $login_name?>".  If you are that someone, follow this link to 
 complete the process:
 
-<?= 
+<?php echo  
 url::site('verifyemail') . '?' . http_build_query(array(
     'email_verification_token' => $email_verification_token
 ))
--- application/views/auth_profiles/register_email.php
+++ application/views/auth_profiles/register_email.php
@@ -1,9 +1,9 @@
 From: l.m.orchard@gmail.com
-Subject: New user registration for <?=$login_name?>.
+Subject: New user registration for <?php echo $login_name?>.
 
-Someone (possibly you) has registered for a login named "<?=$login_name?>".  If you are that someone, follow this link to verify your email address and complete the process:
+Someone (possibly you) has registered for a login named "<?php echo $login_name?>".  If you are that someone, follow this link to verify your email address and complete the process:
 
-<?= 
+<?php echo  
 url::site('verifyemail') . '?' . http_build_query(array(
     'email_verification_token' => $email_verification_token
 ))
