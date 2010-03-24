9623d1b6b5ae16e343917e73e73c11f5c77d3668
aking@mozilla.com <aking@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-03-24 17:30:48 +0000

    Bug#554571 - Checking in a very basic error page. We can iterate to improve.
    git-svn-id: https://svn.mozilla.org/projects/plugindir/trunk@64715 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ application/views/kohana_error_page.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Ouch</title>
+<base href="http://php.net/" />
+</head>
+<body>
+<div id="framework_error" style="width:42em;margin:20px auto;">
+<h3>Ouch</h3>
+<style type="text/css">
+.south {
+    -moz-transform: rotate(30deg);
+    -moz-transform-origin: left top;
+    
+    -moz-transition-duration: 4s;  
+    -moz-transition-delay: 2s;
+
+   -moz-transition-property: background-color, color, left;  
+
+   -moz-transition-duration: 5s;  
+   -webkit-transition-property: background-color, color, left;  
+   -webkit-transition-duration: 5s;  
+   -o-transition-property: background-color, color, left;  
+   -o-transition-duration: 5s;  
+}
+</style>
+<p class="south">Something went south...</p>
+</div>
+</body>
+</html>
