b68a7ea4a938422f394ed176660742ca947e3d0a
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-07-15 13:43:18 -0400

    bug 553678: Oops - that was a filename, not SQL

--- application/config/sql/migrations/20100715000-bug-553678-audit-trail.sql
+++ application/config/sql/migrations/20100715000-bug-553678-audit-trail.sql
@@ -1,1 +1,20 @@
-application/config/sql/migrations/20100715000-bug-553678-audit-trail.sql
+--
+-- bug 553678: create audit trail
+--
+DROP TABLE IF EXISTS `activity_log`;
+CREATE TABLE `activity_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profile_id` int(11),
+  `action` varchar(255) NOT NULL,
+  `pfs_id` char(64),
+  `plugin_id` int(11) unsigned,
+  `pluginrelease_id` int(11) unsigned,
+  `details` text,
+  `old_state` text,
+  `new_state` text,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `activity_log_ibfk_2` (`profile_id`),
+  KEY `activity_log_ibfk_3` (`plugin_id`),
+  KEY `activity_log_ibfk_4` (`pluginrelease_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
