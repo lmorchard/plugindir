7fffa23a5e5478b9c2beb6e9729769031b8ef7bc
ossreleasefeed <sneethling@mozilla.com>
2014-04-11 11:18:15 +0200

    Fix Bug 956905, create JSON list of all plugins in database

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,6 +4,8 @@ $config['pfs/v2'] = 'search/pfs_v2';
 
 $config['index.json'] = 'plugins/index_json';
 
+$config['plugins_list.json'] = 'plugins/plugins_list';
+
 $config['plugins/?;create'] = 'plugins/create';
 
 $config['plugins/detail/([^\.]+).json'] = 'plugins/detail/$1/json';
@@ -15,15 +17,15 @@ $config['profiles/([^/]+)/']             = 'plugins/sandbox/$1';
 $config['profiles/([^/]+)/plugins/']     = 'plugins/sandbox/$1';
 $config['profiles/([^/]+)/plugins.json'] = 'plugins/sandbox/$1/json';
 
-$config['profiles/([^/]+)/plugins/?;create'] = 
+$config['profiles/([^/]+)/plugins/?;create'] =
     'plugins/create/$1';
-$config['profiles/([^/]+)/plugins/detail/([^\.]+).json'] = 
+$config['profiles/([^/]+)/plugins/detail/([^\.]+).json'] =
     'plugins/detail/$2/json/$1';
-$config['profiles/([^/]+)/plugins/detail/([^\.]+).html'] = 
+$config['profiles/([^/]+)/plugins/detail/([^\.]+).html'] =
     'plugins/detail/$2/html/$1';
-$config['profiles/([^/]+)/plugins/detail/([^;]+);(.*)'] = 
+$config['profiles/([^/]+)/plugins/detail/([^;]+);(.*)'] =
     'plugins/$3/$2/$1';
-$config['profiles/([^/]+)/plugins/detail/(.*)'] = 
+$config['profiles/([^/]+)/plugins/detail/(.*)'] =
     'plugins/detail/$2/html/$1';
 
 $config['plugins/submissions/(.*)'] = 'plugins/submission_detail/$1';
@@ -37,9 +39,9 @@ $config['register'] = 'auth_profiles/register';
 $config['login'] =    'auth_profiles/login';
 $config['logout'] =   'auth_profiles/logout';
 
-$config['verifyemail'] = 
+$config['verifyemail'] =
     'auth_profiles/verifyemail';
-$config['reverifyemail/(.*)'] = 
+$config['reverifyemail/(.*)'] =
     'auth_profiles/reverifyemail/login_name/$1/';
 
 $config['changepassword'] = 'auth_profiles/changepassword';
@@ -47,16 +49,16 @@ $config['forgotpassword'] = 'auth_profiles/forgotpassword';
 
 $config['settings'] = 'auth_profiles/settings';
 
-$config['profiles/([^/]+)/settings'] = 
+$config['profiles/([^/]+)/settings'] =
     'auth_profiles/settings/screen_name/$1/';
 
-$config['profiles/([^/]+)/settings/basics/changepassword'] = 
+$config['profiles/([^/]+)/settings/basics/changepassword'] =
     'auth_profiles/changepassword/screen_name/$1/';
 
-$config['profiles/([^/]+)/settings/basics/changeemail'] = 
+$config['profiles/([^/]+)/settings/basics/changeemail'] =
     'auth_profiles/changeemail/screen_name/$1/';
 
-$config['profiles/([^/]+)/settings/basics/details'] = 
+$config['profiles/([^/]+)/settings/basics/details'] =
     'auth_profiles/editprofile/screen_name/$1/';
 
 $config['captcha/?(.*)'] = 'index/captcha/$1';
